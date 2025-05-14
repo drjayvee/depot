51fa1cfc8c1e6e1b70a645d073e151920b2c3f84
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-05-14 20:41:30 +0200

    Add Cart#empty?

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,7 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def empty?
+    line_items.empty?
+  end
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "empty?" do
+    cart = carts(:two) # this cart is empty
+
+    assert cart.empty?
+
+    cart.line_items.build(product: products(:alaska), cart:)
+
+    refute cart.empty?
+  end
 end
