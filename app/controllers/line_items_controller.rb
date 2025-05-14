30fb02fc5da8cf59a32b2ec418bebd9be7d9b0b7
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-05-14 21:17:49 +0200

    Remove unused Line Item actions methods and views

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,50 +2,20 @@ class LineItemsController < ApplicationController
   include CurrentCart
 
   before_action :set_cart, only: %i[ create ]
-  before_action :set_line_item, only: %i[ show edit update destroy ]
-
-  # GET /line_items or /line_items.json
-  def index
-    @line_items = LineItem.all
-  end
-
-  # GET /line_items/1 or /line_items/1.json
-  def show
-  end
-
-  # GET /line_items/new
-  def new
-    @line_item = LineItem.new
-  end
-
-  # GET /line_items/1/edit
-  def edit
-  end
+  before_action :set_line_item, only: %i[ destroy ]
 
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product:)
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
         format.json { render :show, status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /line_items/1 or /line_items/1.json
-  def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: "Line item was successfully updated." }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
+        @cart.reload
+        format.html { render @cart, status: :unprocessable_entity }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -65,10 +35,5 @@ class LineItemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
       @line_item = LineItem.find(params.expect(:id))
-    end
-
-    # Only allow a list of trusted parameters through.
-    def line_item_params
-      params.expect(line_item: [ :product_id ])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,5 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "store#index", as: "store_index"
 
+  resources :carts
+  resources :line_items, only: %i[ create destroy ]
   resources :products
 end
