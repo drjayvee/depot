2c143dcbdffc4cb8d45c4aa65c406f3544577486
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-05-16 20:03:13 +0200

    Remove unused CartsController actions, rescue from invalid cart error

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,22 +1,9 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy ]
-
-  # GET /carts or /carts.json
-  def index
-    @carts = Cart.all
-  end
+  before_action :set_cart, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts/1 or /carts/1.json
   def show
-  end
-
-  # GET /carts/new
-  def new
-    @cart = Cart.new
-  end
-
-  # GET /carts/1/edit
-  def edit
   end
 
   # POST /carts or /carts.json
@@ -28,7 +15,7 @@ class CartsController < ApplicationController
         format.html { redirect_to @cart, notice: "Cart was successfully created." }
         format.json { render :show, status: :created, location: @cart }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { redirect_to store_index_path, status: :unprocessable_entity }
         format.json { render json: @cart.errors, status: :unprocessable_entity }
       end
     end
@@ -41,7 +28,7 @@ class CartsController < ApplicationController
         format.html { redirect_to @cart, notice: "Cart was successfully updated." }
         format.json { render :show, status: :ok, location: @cart }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { redirect_to @cart, status: :unprocessable_entity }
         format.json { render json: @cart.errors, status: :unprocessable_entity }
       end
     end
@@ -66,5 +53,10 @@ class CartsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def cart_params
       params.fetch(:cart, {})
+    end
+
+    def invalid_cart
+      logger.error "Attempt to access invalid cart: #{params[:id]}"
+      redirect_to store_index_path, notice: "Invalid cart"
     end
 end
