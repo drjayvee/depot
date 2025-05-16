d5ddb93776237a3d94b31ce8e4caa922044f330e
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-05-16 19:37:12 +0200

    Replace Cart#prevent_duplicate_line_items with validation in LineItem
    ... which validates uniqueness of :product_id
    
    I'll keep the test for this behavior in Cart as that feels where the
    responsibility should lie. (Plus it's already there...)

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,6 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  validates :product_id, uniqueness: { scope: :cart_id, message: "already exists in this cart" }
 end
