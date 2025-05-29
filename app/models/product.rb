85412f740648483554daf127647f1ed598b1b6d9
Jeroen Versteeg <3079883+drjayvee@users.noreply.github.com>
2025-05-29 13:45:43 +0200

    Fix Rubocop offenses in Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image do |attachable|
-    attachable.variant :thumb, resize_to_limit: [150, 100], preprocessed: true
-    attachable.variant :preview, resize_to_limit: [640, 480], preprocessed: true
+    attachable.variant :thumb,   resize_to_limit: [ 150, 100 ], preprocessed: true
+    attachable.variant :preview, resize_to_limit: [ 640, 480 ], preprocessed: true
   end
 
   has_many :line_items
