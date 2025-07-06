61c923b9cce5c55dd677a792624a1901f7ec6237
Jeroen Versteeg <jeroen@alisqi.com>
2025-07-06 13:26:55 +0200

    OrdersControllerTest asserts OrderMailer :received is sent

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -45,6 +45,15 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
       } })
     end
 
+    email_order = nil
+    assert_enqueued_email_with OrderMailer, :received,
+      params: ->(params) { email_order = params[:order] }
+
+    assert_equal @order.name, email_order.name
+    assert_equal @order.email, email_order.email
+    assert_equal 1, email_order.line_items.count # @order.line_items is now empty!
+    assert_equal @order.payment.to_json, email_order.payment.to_json
+
     assert_redirected_to order_url(Order.last)
   end
 
