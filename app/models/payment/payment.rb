0922fe06bd30b1a065ed95d2d970afccf7998562
Jeroen Versteeg <jeroen@alisqi.com>
2025-07-13 14:59:48 +0200

    Wrap Payment class methods in class << self block

--- app/models/payment/payment.rb
+++ app/models/payment/payment.rb
@@ -6,34 +6,39 @@ module Payment
     include ActiveModel::Attributes
     include ActiveModel::Serializers::JSON
 
-    def self.new(*)
-      raise NotImplementedError if self == Payment
+    class << self
+      def new(*)
+        raise NotImplementedError if self == Payment
 
-      super
-    end
+        super
+      end
 
-    def self.dump(payment)
-      payment.to_json
-    end
+      # Needed to use this class as a serializer for Order.payment
+      def dump(payment)
+        payment.to_json
+      end
 
-    # Create a Payment from a JSON +string+ or +hash+ and sets attributes.
-    #
-    #   pm = SomePayment.new(attr: "value")
-    #   Payment.load(pm.dump) # => <SomePayment:0x1337, @attr="value">
-    #
-    # Uses the +"type"+ key to determine the subclass and calls +attributes=n+.
-    def self.load(json)
-      return nil if json.nil?
+      # Create a Payment from a JSON +string+ or +hash+ and sets attributes.
+      #
+      #   pm = SomePayment.new(attr: "value")
+      #   Payment.load(pm.dump) # => <SomePayment:0x1337, @attr="value">
+      #
+      # Uses the +"type"+ key to determine the subclass and calls +attributes=n+.
+      #
+      # Also needed for Order.payment
+      def load(json)
+        return nil if json.nil?
 
-      hash = if json.kind_of? String
-               ActiveSupport::JSON.decode(json)
-             else
-               json.stringify_keys
-             end
-      klass = TYPE_CLASSES[hash["type"].to_i]
+        hash = if json.kind_of? String
+                 ActiveSupport::JSON.decode(json)
+               else
+                 json.stringify_keys
+               end
+        klass = TYPE_CLASSES[hash["type"].to_i]
 
-      klass.new.tap do |payment|
-        payment.attributes = hash.filter { payment.attributes.include?(it) }
+        klass.new.tap do |payment|
+          payment.attributes = hash.filter { payment.attributes.include?(it) }
+        end
       end
     end
 
